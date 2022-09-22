cc703aea237608258d5e01129bbf8093c7f2cc7d
Dave Meyer <7davidmeyer@gmail.com>
2022-09-22 17:47:52 +0200

    Refactoring. Added somewhat useful info to readme. Updated manifest data

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
-# Youtube Shorts Remover
+# YouTube Shorts Remover
 
-Copied from https://github.com/mdn/webextensions-examples/tree/master/apply-css
+Firefox Browser extension that removes YouTube Shorts section and YouTube Shorts sidebar entry from YouTube homepage.
 
-# apply-css
+Template copied from [https://github.com/mdn/webextensions-examples/tree/master/apply-css](https://github.com/mdn/webextensions-examples/tree/master/apply-css).
 
-**This add-on injects CSS into web pages. The `addons.mozilla.org` domain disallows this operation, so this add-on will not work properly when it's run on pages in the `addons.mozilla.org` domain.**
+More infos following soon.
 
-## What it does
+--- 
 
-This extension includes:
-
-* a background script, "background.js"
-* a page action
-
-It adds the [page action](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/pageAction)
-to every tab the user opens. Clicking the page action
-for a tab applies some CSS using [tabs.insertCSS](https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/API/tabs/insertCSS).
-
-Clicking again removes the CSS using [tabs.removeCSS](https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/API/tabs/removeCSS).
-
-## What it shows
-
-* some basic page action functions
-* how to apply and remove CSS.
+Dave Meyer liked to create this.
--- shorts-remover.js
+++ shorts-remover.js
@@ -1,11 +1,7 @@
-document.body.style.border = "5px solid red";
-
 const ytShortsSectionElemTagName = "ytd-rich-section-renderer";
 const ytShortsGuideEntryTagName = "ytd-guide-entry-renderer";
 
 const findAncestorElemWithTagName = (elem) => {
-    // alert(elem.tagName.toLowerCase());
-
     if(elem.tagName.toLowerCase() === "body") {
         // iterated to top of body -> element wasn't found
         console.warn("Couldn't find Youtube Shorts Section");
@@ -19,11 +15,8 @@ const findAncestorElemWithTagName = (elem) => {
 
 const potentialShortsSectionHeadlineElems = document.querySelectorAll("span#title.style-scope.ytd-rich-shelf-renderer");
 for(const elem of potentialShortsSectionHeadlineElems) {
-    // alert("innerHTML:" + elem.innerHTML);
     if(elem.innerHTML === "Shorts") {
-        alert("found");
         const parentElem = findAncestorElemWithTagName(elem.parentElement);
-        // alert("parentElem:" + JSON.stringify(parentElem));
         parentElem.style.display = "none";
     }
 }
@@ -31,7 +24,6 @@ for(const elem of potentialShortsSectionHeadlineElems) {
 const potentialShortsGuideEntryHeadlineElems = document.querySelectorAll("yt-formatted-string.title.style-scope.ytd-guide-entry-renderer");
 for(const elem of potentialShortsGuideEntryHeadlineElems) {
     if(elem.innerHTML === "Shorts") {
-        alert("found");
         const parentElem = findAncestorElemWithTagName(elem.parentElement);
         parentElem.style.display = "none";
     }
